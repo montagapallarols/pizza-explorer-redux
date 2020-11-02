77193b2c40d6028380bbfac3e2fcee4fbde1655c
montipallarols <montaga.pallarols@gmail.com>
2020-11-02 18:20:14 +0100

    Set up redux store, reducer and create pizzaList component

---
+++ src/components/PizzaList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+// const selectUser = reduxState => {
+//   return reduxState.user;
+// };
+
+function selectUser(reduxState) {
+    return reduxState.user
+}
+
+export default function PizzaList() {
+    const user = useSelector(selectUser);
+
+  return (
+    <div>
+      <h1>Pizza Explorer</h1>
+      <p>
+        Welcome back, <strong>{user.name}</strong>! Your favorite pizzas:
+      </p>
+      <p>TODO: the list of pizzas</p>
+    </div>
+  );
+}
