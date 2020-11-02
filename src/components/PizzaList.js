dab16381ac447c3e91f68884d75110edcb23ccad
montipallarols <montaga.pallarols@gmail.com>
2020-11-02 20:52:57 +0100

    Define and use selector to get list of pizzas

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-// const selectUser = reduxState => {
-//   return reduxState.user;
-// };
+const selectUser = reduxState => {
+  return reduxState.user;
+};
 
-function selectUser(reduxState) {
-    return reduxState.user
+// function selectUser(reduxState) {
+//     return reduxState.user
+// }
+
+
+// Define a selector to get the list of pizzas, 
+// and then add a line of text showing the total number of known pizzas.
+
+const selectListOfPizzas = reduxState => {
+    return reduxState.pizzas 
+    
 }
+
+// Render the pizzas to an <ul> list with an <li> list item for each pizza, 
+// showing the pizza's name and description as well as number of times it was bought.
 
 export default function PizzaList() {
     const user = useSelector(selectUser);
+    console.log("What is user?", user)
+    const pizzas = useSelector(selectListOfPizzas)
+    console.log("What is pizzas?", pizzas)
 
   return (
     <div>
@@ -18,7 +33,12 @@ export default function PizzaList() {
       <p>
         Welcome back, <strong>{user.name}</strong>! Your favorite pizzas:
       </p>
-      <p>TODO: the list of pizzas</p>
+      <h2>List of pizzas:</h2>
+      <ul>
+        {pizzas.map(pizza => {
+            return <li><strong>{pizza.name}</strong>: <p>{pizza.description}</p></li>
+        })}
+      </ul>
     </div>
   );
 }
